b2b491b2a1ac61ed85813837ea9ab4480ae35896
Yeong Wee <ywee@sas.upenn.edu>
2013-01-23 09:36:30 -0500

    initial commit

---
+++ kickballer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kickballer::Application.config.secret_token = '2ee645ef384e49c4be65b286e4d6159b56730d0c884d6991c1b92051e3b4aa53a692ca1cff8c462bbf722d5d83ad07a5714cb8a7e6ad719ddd4bd44d2c3738a5'
---
+++ myappname/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myappname::Application.config.secret_token = 'fbb9a6191f360a4235394c44f8182a6a1e6997bef9f7ec5a1d27705e6d079491736ba923a3c56f1b8a8dac0faaa5b3628fbb18e0feb9936860e9f13e4946f77d'
